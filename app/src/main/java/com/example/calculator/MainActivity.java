667ac3ea4cd328ed5b9327952b5a532029f2d126
PercentVisionSteal <mary60893590@gmail.com>
2026-05-24 15:51:31 +0000

    Add app/src/main/java/com/example/calculator/MainActivity.java

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setBackgroundColor(0xFF0F1115);
+        webView.loadUrl("file:///android_asset/calculator.html");
+        setContentView(webView);
+        // Edge-to-edge dark UI hint
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
